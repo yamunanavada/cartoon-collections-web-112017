d8c9965c1f11541d2c464336f9b338c90f7686cf
Yamuna Navada <yamunanavada@gmail.com>
2017-11-02 22:38:15 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+
+  array.each_with_index do |name, index|
+    puts "#{index + 1}. #{name}"
+  end
 end
 
 def summon_captain_planet# code an argument here
